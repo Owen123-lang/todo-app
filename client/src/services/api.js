ed03bafd978868d43b14973a594a98135a81fd29
Rowen Rodotua Harahap <150427908+OWENNNN123@users.noreply.github.com>
2025-05-04 13:55:18 +0700

    update api.js
    update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Changed the API URL to match exactly what the backend exposes
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = "https://todo-app-backend-3z4u.onrender.com/api";
+
 
 const api = axios.create({
   baseURL: API_URL,
