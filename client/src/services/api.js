9435bb1cb1c79f0251cf990cd5cc78b1414adeeb
Rowen Rodotua Harahap <150427908+OWENNNN123@users.noreply.github.com>
2025-05-04 14:04:57 +0700

    dw
    daw

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 const API_BASE_URL = "https://todo-app-backend-3z4u.onrender.com/api";
 
 const api = axios.create({
-  baseURL: API_BASE_URL, 
+  baseURL: API_BASE_URL,
 });
-
 
 export const getTodos = async () => {
   try {
